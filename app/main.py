187f597d05fcfdfb73e46b234dfc56df1e56c6e5
anuragshuklajec <anuragshuklajec@gmail.com>
2024-04-29 14:14:58 +0530

    migrating all the sensitive credentials to env file

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from . import models
 from .database import engine
 from .routers import user,post,auth
+from .config import settings
 
 logging.getLogger('passlib').setLevel(logging.ERROR) #supressing a warning that bycrypt raise with newer versions and has nothing to do with logic
 
