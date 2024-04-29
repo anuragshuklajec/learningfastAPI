cf9a148ed9d80137b3d3aa18b4183ada2d926863
anuragshuklajec <anuragshuklajec@gmail.com>
2024-04-29 13:30:28 +0530

    implemented search limit and offset in get post

--- app/routers/post.py
+++ app/routers/post.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI, Response, status,HTTPException, Depends, APIRouter
 from sqlalchemy.orm import Session
-from .. import models,schemas,utils,oauth2
-from .. database import engine, get_db
-from typing import List
+from .. import models,schemas,oauth2
+from .. database import  get_db
+from typing import List,Optional
 
 router = APIRouter(
     prefix = "/posts",
@@ -11,10 +11,11 @@ router = APIRouter(
 )
 
 @router.get('/', response_model = List[schemas.Post])
-def get_posts(db: Session = Depends(get_db)):
+def get_posts(db: Session = Depends(get_db),limit:int = 10,skip:int = 0,search : Optional[str] = ""):
     # cursor.execute(""" SELECT * from posts""")
     # posts = cursor.fetchall()
-    posts = db.query(models.Post).all()
+    print(search)
+    posts = db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
     return posts
 
 @router.post('/',status_code = status.HTTP_201_CREATED, response_model = schemas.Post)
