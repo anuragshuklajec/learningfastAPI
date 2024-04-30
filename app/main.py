c12f973e1b9fc5c856e56479e119a2ffc786ae02
anuragshuklajec <anuragshuklajec@gmail.com>
2024-04-30 16:01:05 +0530

    configuring cors

--- app/main.py
+++ app/main.py
@@ -4,12 +4,24 @@ from . import models
 from .database import engine
 from .routers import user,post,auth,vote
 from .config import settings
+from fastapi.middleware.cors import CORSMiddleware
 
 logging.getLogger('passlib').setLevel(logging.ERROR) #supressing a warning that bycrypt raise with newer versions and has nothing to do with logic
 
 # models.Base.metadata.create_all(bind=engine) to create all the tables in database at start but doesn't recognize changes hence alembic is configured
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
     
 app.include_router(post.router)
 app.include_router(user.router)
